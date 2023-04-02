801283a9d256bda6c7487c9350171bdcd691e46b
Andrei <123077618+AndreiSiminiv@users.noreply.github.com>
2023-04-02 22:48:48 +0300

    вызвать пакеты

---
+++ 1.prostie_dannie/utub/Создание модулей работа со встроеными модулями.py
@@ -0,0 +1,22 @@
+#pupi.org отсюда можно устанавливать пакеты
+# работа со временем
+#import  time
+#time.sleep(3) # заморозить программу указываем на сколько
+#print('Hello')
+
+#import datetime as d
+#print(d.datetime.now().time())
+
+# узнать информацию про пользователя
+import datetime as d, sys, os, platform
+print(platform.system())
+
+
+#
+from math import sqrt as s
+print(s(1000))
+
+
+
+
+
